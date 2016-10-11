1508020d531bf6eb5810413fb1fe3eb9d25caeae
Miguel Angel Ajo Pelayo <miguelangel@ajo.es>
2016-10-11 20:29:29 +0200

    Better initial wifi handling to avoid ESP_xxxxxx AP showing up for fresh devices

--- OHAUTlib/WifiConnection.cpp
+++ OHAUTlib/WifiConnection.cpp
@@ -3,21 +3,19 @@
 #include "consts.h"
 extern ConfigMap configData;
 bool wifi_connected = false;
+
+// TODO(mangelajo): convert this to a proper class...
+
 bool wifiSetup()
 {
   int connect_tries=3;
 
+  WiFi.softAP(configData["wifi_ap_ssid"], configData["wifi_ap_pass"]);
   WiFi.mode(WIFI_STA);
   WiFi.hostname(configData["mqtt_id"]);
-  if (strcmp(configData["wifi_sta_ap"], DEFAULT_STA_AP) == 0 &&
-      strcmp(configData["wifi_sta_pass"], DEFAULT_STA_PASS) == 0) {
-    // if no specific ap/password is set, try first with the SDK
-    // stored settings
-    WiFi.begin();
-  } else {
-    WiFi.begin(configData["wifi_sta_ap"],
-               configData["wifi_sta_pass"]);
-  }
+
+  WiFi.begin(configData["wifi_sta_ap"],
+             configData["wifi_sta_pass"]);
 
   while(--connect_tries && WiFi.waitForConnectResult() != WL_CONNECTED){
     WiFi.begin(configData["wifi_sta_ap"],
@@ -30,7 +28,13 @@ bool wifiSetup()
     WiFi.mode(WIFI_AP);
     WiFi.softAP(configData["wifi_ap_ssid"], configData["wifi_ap_pass"]);
     return false;
+  } else {
+    Serial.printf("WiFi: connected to %s, IP address: %s\r\n",
+            configData["wifi_sta_ap"],
+            WiFi.localIP().toString().c_str());
+
   }
+
   wifi_connected = true;
   return true;
 }
